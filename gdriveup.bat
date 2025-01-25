50e15a5e1b91d00e113c6ec9028fd496b2309ef8
thruse <thruse7@gmail.com>
2025-01-25 03:31:31 +0000

    fixed gdriveup

--- gdriveup.bat
+++ gdriveup.bat
@@ -1,8 +1,7 @@
 @echo off
+goto main
 
-if not exist "%mydir%\tmp\drive_downloads" (
-    mkdir "%mydir%\tmp\drive_downloads"
-)
+:update
 
 pushd "%mydir%\tmp\drive_downloads"
 del /q * > nul 2> nul
@@ -10,4 +9,18 @@ move "%userprofile%\downloads\g-*" .
 rmdir /q /s "%mydir%\g" > nul 2> nul
 call 7z x "*.zip" -o"%mydir%"
 popd
+
+exit /b 0
+
+:main
+
+if not exist "%mydir%\tmp\drive_downloads" (
+    mkdir "%mydir%\tmp\drive_downloads"
+)
+
+if exist "%userprofile%\downloads\g-*" (
+    call :update
+) else (
+    echo error: no drive files found in "%userprofile%\downloads"
+)
 
